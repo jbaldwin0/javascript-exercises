7a379f941553b31ca1c9bc989524a0290a4e87bc
jbaldwin0 <jac0b67@ymail.com>
2022-07-25 17:30:48 -0500

    Complete exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempC = (temp-32)*(5/9);
+  return Math.round(tempC*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempF = (temp*(9/5))+32;
+  return Math.round(tempF*10)/10;
 };
 
 // Do not edit below this line
