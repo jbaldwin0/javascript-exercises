1e7e07d488787d52a750603904ffbd4cc3fced7c
jbaldwin0 <jac0b67@ymail.com>
2022-07-25 15:22:15 -0500

    Complete exercise 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let reString = '';
+    for (let i = 0; i < num; i++) {
+        reString += string;
+    }
+    return reString;
 };
 
 // Do not edit below this line
