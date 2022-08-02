6f5994d90d24e28ecfdeb9c87bd7933b4da4e581
jbaldwin0 <jac0b67@ymail.com>
2022-08-02 18:40:39 -0500

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase().replace(/[^\w\']|_/g, "");
+    let splitStr = input.split("");
+    let revStr = splitStr.reverse();
+    let joinStr = revStr.join("");
+    return joinStr == input ? true : false;
 };
 
 // Do not edit below this line
