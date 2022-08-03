251abf9c3d1573d8d479ad405b56476b02a71d4f
jbaldwin0 <jac0b67@ymail.com>
2022-08-02 23:45:57 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = parseInt(a);
+    if (a < 0) {
+        return "OOPS";
+    }
+    if (isNaN(a)) {
+        return 'error';
+    }
 
+    let previous = -1;
+    let result = 1;
+    let num;
+    for (let i = 0; i <= a; i++) {
+        num = previous + result;
+        previous = result;
+        result = num;
+    }
+    return num;
 };
 
 // Do not edit below this line
