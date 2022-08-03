a47d74b1bceddafd66494ccc0bad2b4c84c68e0b
jbaldwin0 <jac0b67@ymail.com>
2022-08-03 00:32:53 -0500

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if ('yearOfDeath' in person === false){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    const oldest = people.sort((a, b) => a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1 );
+    console.log(oldest);
+    return oldest[0];
 };
 
 // Do not edit below this line
